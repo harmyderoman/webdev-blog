7943937d33a3fcd68841d2d5c2175e00d7a20847
harmyderoman <romangarmider@gmail.com>
2020-10-05 18:54:31 +0300

    articles added _slug.vue

---
+++ pages/_slug.vue
@@ -0,0 +1,60 @@
+<template>
+  <article>
+    <h1>{{ article.title }}</h1>
+    <p>{{ article.description }}</p>
+    <img :src="article.img" :alt="article.alt" />
+    <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
+    <hr />
+
+    <nuxt-content :document="article" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content("articles", params.slug).fetch();
+
+    return {
+      article
+    };
+  },
+  methods: {
+    formatDate(date) {
+      const options = { year: "numeric", month: "long", day: "numeric" };
+      return new Date(date).toLocaleDateString("ru", options);
+    }
+  },
+  computed: {
+    docTitle() {
+      return this.article.title;
+    }
+  },
+  head() {
+    return {
+      title: this.docTitle,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Home page description"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.nuxt-content h2 {
+  font-weight: bold;
+  font-size: 28px;
+}
+.nuxt-content h3 {
+  font-weight: bold;
+  font-size: 22px;
+}
+.nuxt-content p {
+  margin-bottom: 20px;
+}
+</style>
